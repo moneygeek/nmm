b790d2322c74414df61d20fae2c88b2fda19bc8e
Jin Choi <jin.choice@gmail.com>
2013-02-09 19:53:59 -0500

    added messages page

--- nmm/urls.py
+++ nmm/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^design/$', TemplateView.as_view(template_name="design.html"), name='design'),
+    url(r'^messages/$', TemplateView.as_view(template_name="messages.html"), name='messages'),
     # url(r'^nmm/', include('nmm.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
